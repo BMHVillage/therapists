875742d9597a21e92b1751583af67186095eb71f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-10-26 16:35:57 -0500

    Update ecs.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- ecs.php
+++ ecs.php
@@ -76,14 +76,7 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->cacheDirectory(__DIR__ . '/.cache/ecs');
     $ecsConfig->parallel();
-    $ecsConfig->paths([
-        __DIR__ . '/bin',
-        __DIR__ . '/ecs.php',
-        __DIR__ . '/README.md',
-        __DIR__ . '/rector.php',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
+    $ecsConfig->paths([__DIR__]);
 
     $ecsConfig->rules([
         DeclareStrictTypesFixer::class,
